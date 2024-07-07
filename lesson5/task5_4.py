9d9b27545900c1bf192cc1af7b70c4c644d72e38
Georgy Barabanshchikov <g.barabanshchikov@gmail.com>
2024-07-07 18:41:10 +0300

    finish 5_4 task

--- lesson5/task5_4.py
+++ lesson5/task5_4.py
@@ -7,7 +7,60 @@
 # Дополнительно написать функцию, которая будет выводить полученный словарь в виде таблицы
 
 
+
+
+def print_out(dict_users):
+    print(type(dict_users))
+    print("-" * 54)
+    print("|       id | firstname       | secondname      | age |")
+    for idv, user in dict_users.items():
+        print("|", idv, "|", user[0]+" "*(15-len(user[0])), "|", user[1]+" "*(15-len(user[1])), "|", user[2]," |")
+    print("-" * 54)
+
+
+def fix_name(name):
+    if not name.isalpha() or len(name) == 0:
+        return
+    name = name.lower()
+    name = name[0].upper() + name[1:]
+    return name
+
+
+def check_age(age):
+    if (age.isdigit()) and 18 <= int(age) <= 60:
+        return int(age)
+    else:
+        return
+
+
+def fix_id(idv):
+    if (len(idv) > 8) or (len(idv) == 0) or not idv.isdigit():
+        return
+    idv = '0' * (8 - len(idv)) + idv
+    return idv
+
+
+def put_into_dict(fname, sname, age, id):
+    if fname is not None and sname is not None and age is not None and id is not None:
+        if id not in users:
+            users[id] = [fname, sname, age]
+
+
 users = {}
 
 while True:
-    Name =
+    fname = input("Input first name: ")
+    if fname == '':
+        break
+    else:
+        fname = fix_name(fname)
+    sname = fix_name(input("Input second name: "))
+    age = check_age(input("Input age: "))
+    id = fix_id(input("Input id: "))
+
+    put_into_dict(fname, sname, age, id)
+
+print_out(users)
+
+
+
